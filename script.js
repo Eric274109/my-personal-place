3e14102f35c250e26e0e180432f02f5820919e7e
Eric274109 <74148876+Eric274109@users.noreply.github.com>
2021-08-29 19:22:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//nothing here!//
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WF38XX9MEJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WF38XX9MEJ');
+</script>
